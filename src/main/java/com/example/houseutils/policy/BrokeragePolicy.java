ea307986ccd90606a458d39f3c9dc3fb51e35f31
rinikim <tytntu@naver.com>
2022-03-30 10:31:30 +0900

    5강 좋은 코드로 수정 - 함수는 하나의 기능만 가질 수 있도록 분리한다.

---
+++ src/main/java/com/example/houseutils/policy/BrokeragePolicy.java
@@ -0,0 +1,4 @@
+package com.example.houseutils.policy;
+
+public interface BrokeragePolicy {
+}
--- src/main/java/com/example/houseutils/policy/RentBrokeragePolicy.java
+++ src/main/java/com/example/houseutils/policy/RentBrokeragePolicy.java
@@ -1,9 +1,9 @@
 package com.example.houseutils.policy;
 
 /**
- * 매매일 때 중개 수수료를 계산해주는 클래스
+ * 임대차일 때 중개 수수료를 계산해주는 클래스
  */
-public class PurchaseBrokeragePolicy {
+public class RentBrokeragePolicy {
 
     // 해당 함수는 매매와 임대차와 중복되는 것을 알 수 있다.
     public Long calculate(Long price) {
@@ -15,15 +15,15 @@ public class PurchaseBrokeragePolicy {
     private BrokerageRule createBrokerageRule(Long price) {
         BrokerageRule rule;
         if (price < 50000000) {
-            rule = new BrokerageRule(0.6, 250000L);
-        } else if (price < 200000000) {
-            rule = new BrokerageRule(0.5, 800000L);
+            rule = new BrokerageRule(0.5, 200000L);
+        } else if (price < 100000000) {
+            rule = new BrokerageRule(0.4, 300000L);
+        } else if (price < 300000000) {
+            rule = new BrokerageRule(0.3, null);
         } else if (price < 600000000) {
             rule = new BrokerageRule(0.4, null);
-        } else if (price < 900000000) {
-            rule = new BrokerageRule(0.5, null);
         } else {
-            rule = new BrokerageRule(0.9, null);
+            rule = new BrokerageRule(0.8, null);
         }
         return rule;
     }
